243112ab3cc73c244ff0b140b7f6e9bdc21f7e87
Eliot <eliot.andreis@gmail.com>
2026-08-14 14:12:09 +0200

    Clé

---
+++ config..js
@@ -0,0 +1,6 @@
+// À remplir après avoir créé un identifiant OAuth de type "Application Web"
+// dans Google Auth Platform (différent de celui du PC, qui est de type "Desktop app").
+// Voir README.md, section "Mise en place mobile".
+export const CLIENT_ID = "546727727934-n4qmn1vnrra7khns31f1kmt05aljeu22.apps.googleusercontent.com";
+export const GOOGLE_DRIVE_FOLDER_NAME = "Budget App";
+export const GOOGLE_DRIVE_FILE_NAME = "budget.db";
