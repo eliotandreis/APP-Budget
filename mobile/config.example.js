24f0b2d68b1bf4493bf4bf66c813de193ce32af9
Eliot <eliot.andreis@gmail.com>
2026-08-14 14:18:23 +0200

    Delete mobile directory

--- mobile/config.example.js
+++
@@ -1,6 +0,0 @@
-// À remplir après avoir créé un identifiant OAuth de type "Application Web"
-// dans Google Auth Platform (différent de celui du PC, qui est de type "Desktop app").
-// Voir README.md, section "Mise en place mobile".
-export const CLIENT_ID = "REMPLACE_PAR_TON_CLIENT_ID.apps.googleusercontent.com";
-export const GOOGLE_DRIVE_FOLDER_NAME = "Budget App";
-export const GOOGLE_DRIVE_FILE_NAME = "budget.db";
